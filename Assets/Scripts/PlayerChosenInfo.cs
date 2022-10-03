44b87b56f81fc7841bbe0d4e45e7471dde10a6e4
Clara Núñez Millán <claranm00@outlook.es>
2022-10-03 12:33:23 +0200

    Elección de nombre y género implementados
    Se almacenan en PlayerPrefs estas dos elecciones de la pantalla de inicio, y luego se usan en los arhcivos .yarn que son los que contienen el diálogo de la visual novel

--- Assets/Scripts/PlayerChosenInfo.cs
+++ Assets/Scripts/PlayerChosenInfo.cs
@@ -5,20 +5,9 @@ using Yarn.Unity;
 
 public class PlayerChosenInfo : MonoBehaviour
 {
-    private string _name = "name";
-    private string _gender = "none";
-   
-    public void ReadNameInput(string inputText)
-    {
-        _name = inputText;
-        PlayerPrefs.SetString("PlayerName", _name);
-    }
-    public void ReadGenderInput(string DropdownOption)
-    {
-        _gender = DropdownOption;
-        PlayerPrefs.SetString("PlayerGender", _gender);
 
-    }
+   
+
     [YarnFunction("get_player_name")]
     public static string GetPlayerName()
     {
