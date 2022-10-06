82117209332ef99591d1dfac7db452b8e880d25b
ClaraNM <claranm00@outlook.es>
2022-10-06 11:31:15 +0200

    Arreglo del script de stats de NPCs

--- Assets/Scripts/NPC_stats.cs
+++ Assets/Scripts/NPC_stats.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 
-public class Romance_NPC : MonoBehaviour
+public class NPC_stats : MonoBehaviour
 {
-    private string _name;
-    private int _hate;
-    private int _neutral;
-    private int _love;
+    public string name;
+    public int hate;
+    public int neutral;
+    public int _love;
 
 
-    public Romance_NPC(string name, int hate, int neutral, int love)
+    public NPC_stats(string name, int hate, int neutral, int love)
     {
         this.name = name;
-        this._hate = hate;
-        this._neutral = neutral;
+        this.hate = hate;
+        this.neutral = neutral;
         this._love = love;
     }
 
     //Crea las stats de un npc que se van a usar en la visual novel, se almacenan sus datos en playerPrefs con un json
     public void createNPCData(string keyVar, string name)
     {
-        Romance_NPC newCharacter = new Romance_NPC(name, 0, 0, 0);
+        NPC_stats newCharacter = new NPC_stats(name, 0, 0, 0);
         string json = JsonUtility.ToJson(newCharacter);
         PlayerPrefs.SetString(keyVar, json);
     }
@@ -37,24 +37,31 @@ public class Romance_NPC : MonoBehaviour
     public int getNPC_hate(string keyVar)
     {
         string json = PlayerPrefs.GetString(keyVar);
-        Romance_NPC npc = JsonUtility.FromJson<Romance_NPC>(json);
-        return npc._hate;
+        NPC_stats npc = JsonUtility.FromJson<NPC_stats>(json);
+        return npc.hate;
     }
 
     [YarnFunction("get_npc_neutral")]
     public int getNPC_neutral(string keyVar)
     {
         string json = PlayerPrefs.GetString(keyVar);
-        Romance_NPC npc = JsonUtility.FromJson<Romance_NPC>(json);
-        return npc._neutral;
+        NPC_stats npc = JsonUtility.FromJson<NPC_stats>(json);
+        return npc.neutral;
     }
 
     [YarnFunction("get_npc_love")]
     public int getNPC_love(string keyVar)
     {
         string json = PlayerPrefs.GetString(keyVar);
-        Romance_NPC npc = JsonUtility.FromJson<Romance_NPC>(json);
+        NPC_stats npc = JsonUtility.FromJson<NPC_stats>(json);
         return npc._love;
+    }
+
+    public NPC_stats getNPC_stats(string keyVar)
+    {
+        string json = PlayerPrefs.GetString(keyVar);
+        NPC_stats npc = JsonUtility.FromJson<NPC_stats>(json);
+        return npc;
     }
 
     /// <summary>
@@ -67,8 +74,8 @@ public class Romance_NPC : MonoBehaviour
     public void addToHate(string keyVar, int sumValue)
     {
         string json = PlayerPrefs.GetString(keyVar);
-        Romance_NPC npc = JsonUtility.FromJson<Romance_NPC>(json);
-        npc._hate -= sumValue;
+        NPC_stats npc = JsonUtility.FromJson<NPC_stats>(json);
+        npc.hate -= sumValue;
 
         json = JsonUtility.ToJson(npc);
         PlayerPrefs.SetString(keyVar, json);
@@ -79,18 +86,18 @@ public class Romance_NPC : MonoBehaviour
     public void addToNeutral(string keyVar, int sumValue)
     {
         string json = PlayerPrefs.GetString(keyVar);
-        Romance_NPC npc = JsonUtility.FromJson<Romance_NPC>(json);
-        npc._neutral -= sumValue;
+        NPC_stats npc = JsonUtility.FromJson<NPC_stats>(json);
+        npc.neutral -= sumValue;
 
         json = JsonUtility.ToJson(npc);
         PlayerPrefs.SetString(keyVar, json);
     }
 
-    [YarnFunction("add_npc_love)"]
+    [YarnFunction("add_npc_love")]
     public void addToLove(string keyVar, int sumValue)
     {
         string json = PlayerPrefs.GetString(keyVar);
-        Romance_NPC npc = JsonUtility.FromJson<Romance_NPC>(json);
+        NPC_stats npc = JsonUtility.FromJson<NPC_stats>(json);
         npc._love -= sumValue;
 
         json = JsonUtility.ToJson(npc);
