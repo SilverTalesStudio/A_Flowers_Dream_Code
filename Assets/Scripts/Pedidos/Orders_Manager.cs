4b271f0176412489f4a26312286c0310d837a761
BAmado753 <ab574268@gmail.com>
2022-10-22 16:10:26 +0200

    Variables de los pedidos (con valor por defecto de momento)

--- Assets/Scripts/Pedidos/Orders_Manager.cs
+++ Assets/Scripts/Pedidos/Orders_Manager.cs
@@ -20,6 +20,9 @@ public class Orders_Manager : MonoBehaviour
     public TMP_Text Order;
     public TMP_Text Summary;
 
+    public string clienteDelPedido;
+    public string colorDelPedido;
+    public string sentimientoDelPedido;
     
     #endregion
 
@@ -37,19 +40,19 @@ public class Orders_Manager : MonoBehaviour
         }
         else if(character == "Ethan")
         {
-            LigonOrder(order);
+            EthanOrder(order);
         }
-        else if(character == "Cachopan")
+        else if(character == "Allan")
         {
-            CachopanOrder(order);
+            AllanOrder(order);
         }
         else if(character == "Pietro")
         {
             PietroOrder(order);
         }
-        else if(character == "Abuelaentrañable")
+        else if(character == "Sagrario")
         {
-            AbuelaOrder(order);
+            SagrarioOrder(order);
         }
         else
         {
@@ -67,51 +70,91 @@ public class Orders_Manager : MonoBehaviour
             case 1:
                 text_order = "Pedido1 Sophie";
                 text_orderSummary = "ResumenSophie1";
+                clienteDelPedido = "Sophie";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 2:
                 text_order = "Pedido2 Sophie";
                 text_orderSummary = "ResumenSophie2";
+                clienteDelPedido = "Sophie";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 3:
                 text_order = "Pedido3 Sophie";
                 text_orderSummary = "ResumenSophie3";
+                clienteDelPedido = "Sophie";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo"
+
                 break;
 
             case 4:
                 text_order = "Pedido4 Sophie";
                 text_orderSummary = "ResumenSophie4";
+                clienteDelPedido = "Sophie";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 5:
                 text_order = "Pedido5 Sophie";
                 text_orderSummary = "ResumenSophie5";
+                clienteDelPedido = "Sophie";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 6:
                 text_order = "Pedido6 Sophie";
                 text_orderSummary = "ResumenSophie6";
+                clienteDelPedido = "Sophie";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 7:
                 text_order = "Pedido7 Sophie";
                 text_orderSummary = "ResumenSophie7";
+                clienteDelPedido = "Sophie";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 8:
                 text_order = "Pedido8 Sophie";
                 text_orderSummary = "ResumenSophie8";
+                clienteDelPedido = "Sophie";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 9:
                 text_order = "Pedido9 Sophie";
                 text_orderSummary = "ResumenSophie9";
+                clienteDelPedido = "Sophie";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 10:
                 text_order = "Pedido10 Sophie";
                 text_orderSummary = "ResumenSophie10";
+                clienteDelPedido = "Sophie";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
         }
 
@@ -120,58 +163,98 @@ public class Orders_Manager : MonoBehaviour
     }
 
 
-    private void LigonOrder(int order)
+    private void EthanOrder(int order)
     {
         switch (order)
         {
             case 1:
                 text_order = "Pedido1 Ligon";
                 text_orderSummary = "ResumenLigon1";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 2:
                 text_order = "Pedido2 Ligon";
                 text_orderSummary = "ResumenLigon2";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+         
                 break;
 
             case 3:
                 text_order = "Pedido3 Ligon";
                 text_orderSummary = "ResumenLigon3";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 4:
                 text_order = "Pedido4 Ligon";
                 text_orderSummary = "ResumenLigon4";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 5:
                 text_order = "Pedido5 Ligon";
                 text_orderSummary = "ResumenLigon5";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 6:
                 text_order = "Pedido6 Ligon";
                 text_orderSummary = "ResumenLigon6";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 7:
                 text_order = "Pedido7 Ligon";
                 text_orderSummary = "ResumenLigon7";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 8:
                 text_order = "Pedido8 Ligon";
                 text_orderSummary = "ResumenLigon8";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 9:
                 text_order = "Pedido9 Ligon";
                 text_orderSummary = "ResumenLigon9";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 10:
                 text_order = "Pedido10 Ligon";
                 text_orderSummary = "ResumenLigon10";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
         }
 
@@ -180,58 +263,98 @@ public class Orders_Manager : MonoBehaviour
     }
 
 
-    private void CachopanOrder(int order)
+    private void AllanOrder(int order)
     {
         switch (order)
         {
             case 1:
                 text_order = "Pedido1 Cachopan";
                 text_orderSummary = "ResumenCachopan1";
+                clienteDelPedido = "Allan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 2:
                 text_order = "Pedido2 Cachopan";
                 text_orderSummary = "ResumenCachopan2";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 3:
                 text_order = "Pedido3 Cachopan";
                 text_orderSummary = "ResumenCachopan3";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 4:
                 text_order = "Pedido4 Cachopan";
                 text_orderSummary = "ResumenCachopan4";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 5:
                 text_order = "Pedido5 Cachopan";
                 text_orderSummary = "ResumenCachopan5";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 6:
                 text_order = "Pedido6 Cachopan";
                 text_orderSummary = "ResumenCachopan6";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 7:
                 text_order = "Pedido7 Cachopan";
                 text_orderSummary = "ResumenCachopan7";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 8:
                 text_order = "Pedido8 Cachopan";
                 text_orderSummary = "ResumenCachopan8";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 9:
                 text_order = "Pedido9 Cachopan";
                 text_orderSummary = "ResumenCachopan9";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 10:
                 text_order = "Pedido10 Cachopan";
                 text_orderSummary = "ResumenCahopan10";
+                clienteDelPedido = "Ethan";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
         }
 
@@ -247,51 +370,91 @@ public class Orders_Manager : MonoBehaviour
             case 1:
                 text_order = "Pedido1 Pietro";
                 text_orderSummary = "ResumenPietro1";
+                clienteDelPedido = "Pietro";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 2:
                 text_order = "Pedido2 Pietro";
                 text_orderSummary = "ResumenPietro2";
+                clienteDelPedido = "Pietro";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 3:
                 text_order = "Pedido3 Pietro";
                 text_orderSummary = "ResumenPietro3";
+                clienteDelPedido = "Pietro";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 4:
                 text_order = "Pedido4 Pietro";
                 text_orderSummary = "ResumenPietro4";
+                clienteDelPedido = "Pietro";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 5:
                 text_order = "Pedido5 Pietro";
                 text_orderSummary = "ResumenPietro5";
+                clienteDelPedido = "Pietro";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 6:
                 text_order = "Pedido6 Pietro";
                 text_orderSummary = "ResumenPietro6";
+                clienteDelPedido = "Pietro";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 7:
                 text_order = "Pedido7 Pietro";
                 text_orderSummary = "ResumenPietro7";
+                clienteDelPedido = "Pietro";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 8:
                 text_order = "Pedido8 Pietro";
                 text_orderSummary = "ResumenPietro8";
+                clienteDelPedido = "Pietro";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 9:
                 text_order = "Pedido9 Pietro";
                 text_orderSummary = "ResumenPietro9";
+                clienteDelPedido = "Pietro";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 10:
                 text_order = "Pedido10 Pietro";
                 text_orderSummary = "ResumenPietro10";
+                clienteDelPedido = "Pietro";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
         }
 
@@ -300,58 +463,98 @@ public class Orders_Manager : MonoBehaviour
     }
 
 
-    private void AbuelaOrder(int order)
+    private void SagrarioOrder(int order)
     {
         switch (order)
         {
             case 1:
                 text_order = "Pedido1 Abuela";
                 text_orderSummary = "ResumenAbuela1";
+                clienteDelPedido = "Sagrario";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 2:
                 text_order = "Pedido2 Abuela";
                 text_orderSummary = "ResumenAbuela2";
+                clienteDelPedido = "Sagrario";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 3:
                 text_order = "Pedido3 Abuela";
                 text_orderSummary = "ResumenAbuela3";
+                clienteDelPedido = "Sagrario";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 4:
                 text_order = "Pedido4 Abuela";
                 text_orderSummary = "ResumenAbuela4";
+                clienteDelPedido = "Sagrario";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 5:
                 text_order = "Pedido5 Abuela";
                 text_orderSummary = "ResumenAbuela5";
+                clienteDelPedido = "Sagrario";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 6:
                 text_order = "Pedido6 Abuela";
                 text_orderSummary = "ResumenAbuela6";
+                clienteDelPedido = "Sagrario";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 7:
                 text_order = "Pedido7 Abuela";
                 text_orderSummary = "ResumenAbuela7";
+                clienteDelPedido = "Sagrario";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 8:
                 text_order = "Pedido8 Abuela";
                 text_orderSummary = "ResumenAbuela8";
+                clienteDelPedido = "Sagrario";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 9:
                 text_order = "Pedido9 Abuela";
                 text_orderSummary = "ResumenAbuela9";
+                clienteDelPedido = "Sagrario";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
 
             case 10:
                 text_order = "Pedido10 Abuela";
                 text_orderSummary = "ResumenAbuela10";
+                clienteDelPedido = "Sagrario";
+                colorDelPedido = "Algo";
+                sentimientoDelPedido = "Algo";
+
                 break;
         }
 
