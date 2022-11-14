dae842d91b163ad08f5b571146920df0fac36320
BAmado753 <ab574268@gmail.com>
2022-11-14 09:46:37 +0100

    Lunes semana 3 (falta comprobación)

--- Assets/Scripts/FlowesMinigame/FlowerMenu.cs
+++ Assets/Scripts/FlowesMinigame/FlowerMenu.cs
@@ -6,28 +6,29 @@ using TMPro;
 
 public class FlowerMenu : MonoBehaviour
 {
-    public GameObject Text;
     [SerializeField] private Animator animator;
 
     //Esta función es llamada al final de la animación "Clicked"
     //Al abrir el menú se cambia el texto o sprite del botón de abrir y cerrar la lista de flores
     public void OpenMenu()
     {
-        Text.GetComponent<TextMeshPro>().text = ">";
+        if (GetComponent<SpriteRenderer>().flipX == false)
+            GetComponent<SpriteRenderer>().flipX = true;
     }
 
     //Esta función es llamada al final de la animación "CloseClick"
     //Al cerrar el menú se cambia el texto o sprite del botón de abrir y cerrar la lista de flores
     public void CloseMenu()
     {
-         Text.GetComponent<TextMeshPro>().text = "<";
+        if (GetComponent<SpriteRenderer>().flipX == true)
+            GetComponent<SpriteRenderer>().flipX = false;
     }
 
     //Al pulsar el botón
     private void OnMouseDown()
     {
         //Si está cerrada la lista de flores, se activa la animación para abrirla
-        if (Text.GetComponent<TextMeshPro>().text == "<")
+        if (GetComponent<SpriteRenderer>().flipX == false)
         {
             animator.GetComponent<Animator>().Play("Clicked");
         }
@@ -45,5 +46,9 @@ public class FlowerMenu : MonoBehaviour
         GetComponent<SpriteRenderer>().color = Color.gray;
     }
 
+    private void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
 
 }
