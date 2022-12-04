0f8eca51f452ebebeec78a499aece9e307a49c41
Clara Núñez Millán <claranm00@outlook.es>
2022-12-04 20:55:39 +0100

    Versiones custom de scripts del package

---
+++ Assets/Scripts/AlterPackage/AlterOptionView.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+using System;
+
+namespace Yarn.Unity
+{
+    public class AlterOptionView : UnityEngine.UI.Selectable, ISubmitHandler, IPointerClickHandler, IPointerEnterHandler
+    {
+        [SerializeField] TextMeshProUGUI text;
+        [SerializeField] bool showCharacterName = false;
+
+        public Action<DialogueOption> OnOptionSelected;
+
+        DialogueOption _option;
+
+        bool hasSubmittedOptionSelection = false;
+
+        public DialogueOption Option
+        {
+            get => _option;
+
+            set
+            {
+                _option = value;
+
+                hasSubmittedOptionSelection = false;
+
+                // When we're given an Option, use its text and update our
+                // interactibility.
+                if (showCharacterName)
+                {
+                    text.text = value.Line.Text.Text;
+                }
+                else
+                {
+                    text.text = value.Line.TextWithoutCharacterName.Text;
+                }
+                interactable = value.IsAvailable;
+            }
+        }
+
+        // If we receive a submit or click event, invoke our "we just selected
+        // this option" handler.
+        public void OnSubmit(BaseEventData eventData)
+        {
+            InvokeOptionSelected();
+        }
+
+        public void InvokeOptionSelected()
+        {
+            // We only want to invoke this once, because it's an error to
+            // submit an option when the Dialogue Runner isn't expecting it. To
+            // prevent this, we'll only invoke this if the flag hasn't been cleared already.
+            if (hasSubmittedOptionSelection == false)
+            {
+                OnOptionSelected.Invoke(Option);
+                hasSubmittedOptionSelection = true;
+            }
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            InvokeOptionSelected();
+        }
+
+        // If we mouse-over, we're telling the UI system that this element is
+        // the currently 'selected' (i.e. focused) element. 
+        public override void OnPointerEnter(PointerEventData eventData)
+        {
+            base.OnSelect(eventData);
+        }
+        public override void OnPointerExit(PointerEventData eventData)
+        {
+            base.OnDeselect(eventData);
+        }
+
+    }
+}
